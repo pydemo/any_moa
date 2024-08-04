3da005464b90e483a7c9daed16a6cd6e4319218b
alex buz <alex_buz@yahoo.com>
2024-08-04 10:22:25 -0400

    anthropic

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ import include.api.mistral as mistral
 import include.api.nvidia as nvidia
 import include.api.deepseek as deepseek
 import include.api.hugging_face as hugging_face
+import include.api.anthropic as anthropic
 e=sys.exit
 
 
--- include/api/anthropic.py
+++ include/api/anthropic.py
@@ -1,38 +1,45 @@
 import os, time
 import asyncio
-import groq
-from groq import AsyncGroq, Groq
+
+from anthropic import AsyncAnthropic, RateLimitError
 from include.common import get_final_system_prompt
 from pprint import pprint as pp
 
 
 
-
-class AsyncClient(AsyncGroq):   
+class AsyncClient(AsyncAnthropic):   
     def __init__(self, api_key):
         super().__init__(api_key=api_key)
 
 async def get_final_stream(client,aggregator_model,user_prompt,  results):
     sys_prompt = get_final_system_prompt( results)
-    
-    final_stream = await client.chat.completions.create(
-        model=aggregator_model,
-        messages=[
-            {
-                "role": "system",
-                "content": sys_prompt,
-            },
-            {"role": "user", "content": user_prompt},
-        ],
-        stream=True,
-    )
-    async for chunk in final_stream:
-        yield chunk.choices[0].delta.content or ""
-    
+    for sleep_time in [1, 2, 4]:
+        try:
+
+
+            async with client.messages.stream(
+                 model=aggregator_model,
+                max_tokens=1024,
+                system=sys_prompt,
+                messages=[
+             
+                    {"role": "user", "content": user_prompt},
+                ],
+            ) as final_stream:
+                async for text in final_stream.text_stream:
+                    yield text
+            break
+
+        except RateLimitError as e:
+            print(e)
+            await asyncio.sleep(sleep_time)   
+
+
+
 
 async def run_llm(client, layer, model, user_prompt,prev_response=None):
     """Run a single LLM call with a model while accounting for previous responses + rate limits."""
-    print(f'\t{layer}:      groq: run_llm:', model)
+    print(f'\t{layer}:  anthropic: run_llm:', model)
     sys_prompt = None
     if prev_response:
         sys_prompt = get_final_system_prompt(  prev_response)
@@ -40,28 +47,32 @@ async def run_llm(client, layer, model, user_prompt,prev_response=None):
 
     for sleep_time in [1, 2, 4]:
         try:
+            system={'system':sys_prompt}   if prev_response else {}
             messages = (
                 [
-                    {
-                        "role": "system",
-                        "content": sys_prompt,
-                    },
+
                     {"role": "user", "content": user_prompt},
                 ]
                 if prev_response
                 else [{"role": "user", "content": user_prompt}]
             )
-            response = await client.chat.completions.create(
+            #pp(messages)
+            response = await client.messages.create(
                 model=model,
+                **system,
                 messages=messages,
                 temperature=0.7,
-                max_tokens=512,
+                max_tokens=1024,
             )
+
+
+            
+
             print(f"\t\t{layer}:     groq: Sleep: {sleep_time}: Model: ", model)
             break
-        except groq.RateLimitError as e:
+        except RateLimitError as e:
             print(e)
             await asyncio.sleep(sleep_time)
-    assert response.choices[0].message.content
-    print(f'\t  {layer}:','groq'.rjust(10,' '),f':{model}:Content:', response.choices[0].message.content[:50])
-    return response.choices[0].message.content
+    assert response.content
+    print(f'\t  {layer}:','anthropic'.rjust(10,' '),f':{model}:Content:', len(response.content), response.content[0].text[:50])
+    return response.content[0].text
